cfdcf3cd7b8fa17dd2f76f894d0b721adf54f964
Frank Cipolla <frankc@cscit.net>
2019-01-02 15:42:00 -0500

    Initial ImaggaRouter Commit

---
+++ ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,19 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by Frank Cipolla on 12/27/18.
+//  Copyright © 2018 Frank Cipolla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
---
+++ ImaggaRouter/README.txt
@@ -0,0 +1,15 @@
+README.txt for ImaggaRouter portion of the 'Dependency Management Using Git Submodules' tutorial application
+
+https://www.raywenderlich.com/690-dependency-management-using-git-submodules
+
+Working Directory:
+/Users/frankcipolla/Workspace/Book_Tutorials/GitTutorials/ImaggaRouter/ImaggaRouter/README.txt
+
+
+
+
+
+
+
+
+
